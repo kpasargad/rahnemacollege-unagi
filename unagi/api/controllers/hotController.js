b7cccd880d87f65480f000e72eaeb7f2862694d3
Mahbodmajid <mahbod.majid@gmail.com>
2017-08-02 12:20:18 +0430

    Change formula to use seconds instead of milliseconds

--- unagi/api/controllers/hotController.js
+++ unagi/api/controllers/hotController.js
@@ -10,7 +10,7 @@ var hotness = function (post, callback) {
     //TODO: handle errors of unavailable posts.
     var likes = post.number_of_likes;
     console.log("NUMBER OF LIKES = ",likes);
-    var date = post.timestamp;
+    var date = post.timestamp /1000;
     var order = Math.log10(Math.max(likes, 1), 10);
     var seconds = date - 1134028003;
     callback(Math.round(order + seconds / 45000, 7));
@@ -20,7 +20,7 @@ exports.hotness = hotness;
 var hotnessBaseValue = function(date){
     var likes = 0;
     console.log("NUMBER OF LIKES = ",likes);
-    var date = date
+    var date = date / 1000;
     var order = Math.log10(Math.max(likes, 1), 10);
     var seconds = date - 1134028003;
     return Math.round(order + seconds / 45000, 7);
