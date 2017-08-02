246717f5d92ce3238db241e3be292c747b0e30f0
kpasargad <kpasargad2@gmail.com>
2017-08-02 12:29:25 +0430

    Fix hotness formula round function

--- unagi/api/controllers/hotController.js
+++ unagi/api/controllers/hotController.js
@@ -9,21 +9,21 @@ var mongoose = require('mongoose'),
 var hotness = function (post, callback) {
     //TODO: handle errors of unavailable posts.
     var likes = post.number_of_likes;
-    console.log("NUMBER OF LIKES = ",likes);
-    var date = post.timestamp /1000;
+    console.log("NUMBER OF LIKES = ", likes);
+    var date = post.timestamp / 1000;
     var order = Math.log10(Math.max(likes, 1), 10);
     var seconds = date - 1134028003;
-    callback(Math.round(order + seconds / 45000, 7));
+    callback(Math.round((order + seconds / 45000) * 10000000) / 10000000);
 };
 exports.hotness = hotness;
 
-var hotnessBaseValue = function(date){
+var hotnessBaseValue = function (date) {
     var likes = 0;
-    console.log("NUMBER OF LIKES = ",likes);
+    console.log("NUMBER OF LIKES = ", likes);
     var date = date / 1000;
     var order = Math.log10(Math.max(likes, 1), 10);
     var seconds = date - 1134028003;
-    return Math.round(order + seconds / 45000, 7);
+    return Math.round((order + seconds / 45000) * 10000000) / 10000000;
 };
 exports.hotnessBaseValue = hotnessBaseValue;
 
